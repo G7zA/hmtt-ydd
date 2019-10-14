cb49e0472ba8f150b13e26efbdda7f1b57eec825
gqz <815138383@qq.com>
2019-10-14 09:14:55 +0800

    展示文章列表（先使用模拟数据）每个频道都有自己的文章列表

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,25 +5,24 @@
     <!-- 导航栏 -->
     <!-- 频道列表 -->
     <van-tabs v-model="active">
-      <van-tab
-      :title="channel.name"
-      v-for="channel in channels" :key="channel.id">
-        <!-- 文章列表 -->
+      <van-tab :title="channel.name" v-for="channel in channels" :key="channel.id">
+        <!-- 文章列表 1.1.2 然后在让文章列表绑定每个频道对应的数据-->
         <van-list
-          v-model="loading"
-          :finished="finished"
+          v-model="channel.loading"
+          :finished="channel.finished"
           finished-text="没有更多了"
-           @load="onLoad">
-          <van-cell
-            v-for="item in list"
-            :key="item"
-            :title="item" />
+          @load="onLoad"
+        >
+          <van-cell v-for="item in channel.articles" :key="item" :title="item" />
+          <!-- channel.articles 该频道的文章列表
+          channel.loading 该频道的加载状态
+          channel.finished 该频道的结束状态（加载完成）-->
         </van-list>
         <!-- 文章列表 -->
       </van-tab>
       <!-- <van-tab title="标签 2">内容 2</van-tab>
       <van-tab title="标签 3">内容 3</van-tab>
-      <van-tab title="标签 4">内容 4</van-tab> -->
+      <van-tab title="标签 4">内容 4</van-tab>-->
     </van-tabs>
     <!-- 频道列表 -->
   </div>
@@ -47,24 +46,39 @@ export default {
   },
   methods: {
     onLoad () {
+      // 1.1.3 修改函数 将原来的属性覆盖，重新赋值成新定义的数据
+      //   获取当前频道
+      const activeChannel = this.channels[this.active]
       // 异步更新数据
       setTimeout(() => {
+        // 1. 请求获取数据
         for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
+          // 2. 将数据添加到 当前频道的articles 中
+          activeChannel.articles.push(activeChannel.articles.length + 1)
         }
-        // 加载状态结束
-        this.loading = false
+        // 3.加载状态结束
+        activeChannel.loading = false
 
+        // 4. 判断数据是否已全部加载结束，如果没有数据了，将 finish 设置为 true
         // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
+        if (activeChannel.articles.length >= 40) {
+          activeChannel.finished = true
         }
       }, 500)
     },
     // 获取默认推荐频道列表方法
     async getChannels () {
       const { data } = await defaultChannels()
-      console.log(data)
+      //   console.log(data)
+
+      //  1.展示文章列表 用模拟数据
+      // 1.1因为每个频道都有一份儿自己的文章列表数据，所以我们首先要将数据进行改造，为每个频道添加自定义数据：文章列表、loading状态、finished 结束状态
+      // 1.1.1定制频道的内容数据
+      data.data.channels.forEach(channel => {
+        channel.articles = [] // 频道的文章列表
+        channel.loading = false // 频道的上拉加载更多的 loading 状态
+        channel.finished = false // 频道的加载结束的状态
+      })
       this.channels = data.data.channels // 将服务器中的数据赋值给data中的channels中 然后做循环遍历渲染到页面上
     }
   }
